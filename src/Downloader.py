d1d8a9f9eba1a925b13bb699e8030ec1385b4585
alirezamhm <alireza.am1379@gmail.com>
2023-03-10 21:45:00 +0330

    add progressbar

--- src/Downloader.py
+++ src/Downloader.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
+import progressbar
 import requests
 
 FORMATS = ('.mkv', '.mp4')
@@ -28,6 +29,19 @@ for link in soup.find_all('a'):
         
 print(f"Number of videos found: {len(video_hrefs)}")
 
+pbar =  None
+def show_progress(block_num, block_size, total_size):
+    global pbar
+    if pbar is None:
+        pbar = progressbar.ProgressBar(maxval=total_size)
+        pbar.start()
+    downloaded = block_num * block_size
+    if downloaded < total_size:
+        pbar.update(downloaded)
+    else:
+        pbar.finish()
+        pbar = None
+
 output_dir = args.Output if args.Output else "./local"
 os.makedirs(output_dir, exist_ok=True)
 for href in video_hrefs:
@@ -38,7 +52,7 @@ for href in video_hrefs:
         else:
             video_url = url + href
         file_name = href.split("/")[-1].replace("%20", " ")
-        urlretrieve(video_url.replace(" ", "%20"), os.path.join(output_dir, file_name))
+        urlretrieve(video_url.replace(" ", "%20"), os.path.join(output_dir, file_name), show_progress)
     except Exception as e:
         print(f"Exception occured \n{e}")
 print("Download finished.")
